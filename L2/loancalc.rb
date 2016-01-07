191215605bbeb2bfd09d68dc8ed0e1c8a7044718
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-07 02:47:30 -0800

    Solicit loan amount and APR

--- L2/loancalc.rb
+++ L2/loancalc.rb
@@ -29,4 +29,62 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+# Display a prompt and get a string
+def solicit(message)
+  prompt(message)
+  gets.chomp
+end
+
+# Returns true if #{value} can be properly represented as a floating point
+# number, false otherwise.
+def valid_float?(value)
+  Float value
+rescue ArgumentError
+  false
+end
+
+# Returns true if #{value} can be properly represented as a non-negative
+# floating point number, false otherwise.
+def valid_non_negative_float?(value)
+  valid_float?(value) && value.to_f >= 0.00
+end
+
+# Returns true if #{value} can be properly represented as a dollar amount.
+def valid_dollar_amount?(value)
+  valid_non_negative_float? value
+end
+
+# Return true if #{value} can be properly represented as an APR between 0.00
+# and 1.00, false otherwise.
+def valid_apr?(value)
+  valid_non_negative_float?(value) && value.to_f <= 1.00
+end
+
+def solicit_loan_amount
+  loop do
+    amount = solicit MESSAGES['enter_amount']
+    return amount if valid_dollar_amount? amount
+    prompt MESSAGES['bad_amount']
+  end
+end
+
+def solicit_apr
+  loop do
+    apr = solicit MESSAGES['enter_apr']
+    return apr if valid_apr? apr
+    prompt MESSAGES['bad_apr']
+  end
+end
+
+def calculate_loan_payment
+  amount = solicit_loan_amount
+  apr = solicit_apr
+  puts "amount = #{amount}   apr = #{apr}"
+end
+
 prompt MESSAGES['welcome']
+loop do
+  calculate_loan_payment
+  answer = solicit MESSAGES['try_again']
+  break unless answer.downcase.start_with? MESSAGES['yes']
+end
