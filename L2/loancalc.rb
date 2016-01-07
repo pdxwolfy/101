51082eb54f7bb970cf9fb355f35f9655f6b2ee4f
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-07 02:15:07 -0800

    Show welcome banner

--- L2/loancalc.rb
+++ L2/loancalc.rb
@@ -6,15 +6,27 @@
 #         P = L -------------
 #               (1 + c)^n - 1
 #
-#    c - monthly interest rate = APR / 12
+#    c - monthly interest rate = APR / 12 scaled to 0 .. 1
 #    n - loan duration in months
 #    L - total loan amount
 #    P - monthly payment
 #
 # Process:
 #    Solicit loan amount (L) in dollars
-#    Solicit APR (i) as a number between 0.0 and 1.0.
+#    Solicit APR (i) as a number between 0 and 100.
 #    Solicit loan duration (n) in months
 #    Calculate c = APR / 12
 #    Calculate P = ....
 #    Display results
+
+require 'yaml'
+
+LANGUAGE = 'en'
+MESSAGES = YAML.load_file('loancalc.yml')[LANGUAGE]
+
+# Display a prompt
+def prompt(message)
+  puts "=> #{message}"
+end
+
+prompt MESSAGES['welcome']
