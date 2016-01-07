8f364369631b379f7d4d3ae2516f9d370c725a97
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-06 22:06:00 -0800

    Offload messages to text file.

--- L2/calculator.rb
+++ L2/calculator.rb
@@ -1,3 +1,5 @@
+load 'messages.en'
+
 # Display a prompt
 def prompt(message)
   puts "=> #{message}"
@@ -13,59 +15,57 @@ end
 # Convert operation shorthand to an -ing verb.
 def operation_verbing(operator)
   lookup_table = {
-    '1' => 'Adding',
-    '2' => 'Subtracting',
-    '3' => 'Multiplying',
-    '4' => 'Dividing'
+    '1' => MESSAGES[:ADDING],
+    '2' => MESSAGES[:SUBTRACTING],
+    '3' => MESSAGES[:MULTIPLYING],
+    '4' => MESSAGES[:DIVIDING]
   }
 
   lookup_table[operator]
 end
 
-prompt "Welcome to Calculator. Enter your name:"
+prompt MESSAGES[:WELCOME]
 
 name = ''
 loop do
   name = gets.chomp
   break unless name.empty?
-  prompt "Make sure you use your real name."
+  prompt MESSAGES[:USE_REAL_NAME]
 end
 
-prompt "Hi #{name}!"
+prompt MESSAGES[:HELLO] % { name: name }
 
 loop do
   number1 = ''
   loop do
-    prompt "What's the first number?"
+    prompt MESSAGES[:FIRST_NUMBER]
     number1 = gets.chomp
     break if number? number1
-    prompt "#{number1} does not look like a valid number"
+    prompt MESSAGES[:BAD_NUMBER]
   end
 
   number2 = ''
   loop do
-    prompt "What's the second number?"
+    prompt MESSAGES[:SECOND_NUMBER]
     number2 = gets.chomp
     break if number? number2
-    prompt "#{number2} does not look like a valid number"
+    prompt MESSAGES[:BAD_NUMBER]
   end
 
   operator = ''
-  prompt <<-EOS
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
-  EOS
-
+  prompt MESSAGES[:WHAT_OP]
   loop do
     operator = gets.chomp
     break if %(1 2 3 4).include? operator
-    prompt "Please enter 1, 2, 3, or 4"
+    prompt MESSAGES[:BAD_OP]
   end
 
-  prompt "#{operation_verbing operator} #{number1} and #{number2}"
+  prompt MESSAGES[:WILL_DO] % {
+    operating: operation_verbing(operator),
+    number1: number1,
+    number2: number2
+  }
+
   result =
     case operator
     when '1'
@@ -79,10 +79,10 @@ loop do
       number1.to_f / number2.to_f
     end
 
-  prompt "The result is #{result}"
-  prompt "Do you want to perform another calculation (Y for yes)?"
+  prompt MESSAGES[:RESULT_IS] % { result: result }
+  prompt MESSAGES[:ANOTHER_CALC]
   answer = gets.chomp.downcase
-  break unless answer.start_with? 'y'
+  break unless answer.start_with? MESSAGES[:YES]
 end
 
-prompt "Thank you for using Calculator. Bye!"
+prompt MESSAGES[:THANK_YOU]
