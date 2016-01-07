0534d9a6cd00f87f79d3bcfb73298fceec7f986c
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-06 16:35:01 -0800

    Alternate formatting of case

--- L2/calculator.rb
+++ L2/calculator.rb
@@ -31,16 +31,17 @@ end
 prompt "What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide"
 operator = gets.chomp
 
-result = case operator
-         when '1'
-           number1.to_i + number2.to_i
-         when '2'
-           number1.to_i - number2.to_i
-         when '3'
-           number1.to_i * number2.to_i
-         when '4'
-           # Warn: possible divide by 0 if 0 input is allowed
-           number1.to_f / number2.to_f
-         end
+result =
+  case operator
+  when '1'
+    number1.to_i + number2.to_i
+  when '2'
+    number1.to_i - number2.to_i
+  when '3'
+    number1.to_i * number2.to_i
+  when '4'
+    # Warn: possible divide by 0 if 0 input is allowed
+    number1.to_f / number2.to_f
+  end
 
 prompt "The result is #{result}"
