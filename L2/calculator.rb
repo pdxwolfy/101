50a3786bcb77e71e07dbe21331bc9fe94907c429
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-06 16:29:46 -0800

    Loop over number input to validate numeric input.

--- L2/calculator.rb
+++ L2/calculator.rb
@@ -3,13 +3,30 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+# Return true if #{value} is a valid number, false otherwise.
+# Currently returns false is #{value} is "0" or equivalent value.
+# Currently returns true if #{value} just starts with a number
+def valid_number?(value)
+  value.to_i != 0
+end
+
 prompt "Welcome to Calculator"
 
-prompt "What's the first number?"
-number1 = gets.chomp
+number1 = ""
+loop do
+  prompt "What's the first number?"
+  number1 = gets.chomp
+  break if valid_number? number1
+  prompt "#{number1} does not look like a valid number"
+end
 
-prompt "What's the second number?"
-number2 = gets.chomp
+number2 = ""
+loop do
+  prompt "What's the second number?"
+  number2 = gets.chomp
+  break if valid_number? number2
+  prompt "#{number2} does not look like a valid number"
+end
 
 prompt "What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide"
 operator = gets.chomp
