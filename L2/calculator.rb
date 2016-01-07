c7d4503f0b9a5930e0c73ece140b1e94820325da
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-06 16:33:13 -0800

    Refactor large calculation if as case

--- L2/calculator.rb
+++ L2/calculator.rb
@@ -31,14 +31,16 @@ end
 prompt "What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide"
 operator = gets.chomp
 
-if operator == '1'
-  result = number1.to_i + number2.to_i
-elsif operator == '2'
-  result = number1.to_i - number2.to_i
-elsif operator == '3'
-  result = number1.to_i * number2.to_i
-elsif operator == '4'
-  result = number1.to_f / number2.to_f
-end
+result = case operator
+         when '1'
+           number1.to_i + number2.to_i
+         when '2'
+           number1.to_i - number2.to_i
+         when '3'
+           number1.to_i * number2.to_i
+         when '4'
+           # Warn: possible divide by 0 if 0 input is allowed
+           number1.to_f / number2.to_f
+         end
 
 prompt "The result is #{result}"
