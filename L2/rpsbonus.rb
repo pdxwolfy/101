b2e491ace1023469849e5d6a47801c2844613701
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-07 18:35:07 -0800

    Ready to go.

--- L2/rpsbonus.rb
+++ L2/rpsbonus.rb
@@ -1,18 +1,45 @@
-VALID_CHOICES = {
-  'r' => 'rock',
-  'p' => 'paper',
+# Rock, Paper, Scissors, Lizard, Spock game.
+# Pete Hanson
+
+VALID_CHOICES = { # abbreviation => full value
+  'r'  => 'rock',
+  'p'  => 'paper',
   'sc' => 'scissors',
   'sp' => 'spock',
-  'l' => 'lizard'
+  'l'  => 'lizard'
 }
 
 WINNING_COMBOS = { # my choice => computer choice
-  'rock' => %w(scissors lizard),
-  'paper' => %w(rock spock),
+  'rock'     => %w(scissors lizard),
+  'paper'    => %w(rock spock),
   'scissors' => %w(paper lizard),
-  'lizard' => %w(paper spock),
-  'spock' => %w(scissors rock)
+  'lizard'   => %w(paper spock),
+  'spock'    => %w(scissors rock)
 }
+
+MESSAGES = {
+  abbreviations:  <<-EOS,
+You may abbreviate choices as follows:
+    r  -> rock
+    p  -> paper
+    sc -> scissors
+    l  -> lizard
+    sp -> spock
+  EOS
+  you_won:        'You won this round.',
+  computer_won:   'Computer won this round.',
+  tie:            'Tie: nobody won this round.',
+  welcome:        'Welcome to Rock, Paper, Scissors, Lizard, Spock!'
+}
+
+WINNING_SCORE = 5  # First player to this score wins.
+
+#-----------------------------------------------------------------------------
+# Returns true if the game is over, false otherwise.
+
+def game_over?(scores)
+  scores.any? { |_, score| score >= WINNING_SCORE }
+end
 
 #-----------------------------------------------------------------------------
 # Display a prompt or other message
@@ -21,15 +48,15 @@ def prompt(message)
 end
 
 #-----------------------------------------------------------------------------
-# Returns a string that describes whether the play won, the computer won, or
-# there was a tie game.
+# Returns a symbol (:you_won, :computer_won, or :tie) depending on whether the
+# player or computer won this round.
 def results(player, computer)
   if win?(player, computer)
-    'You won!'
+    :you_won
   elsif win?(computer, player)
-    'Computer won!'
+    :computer_won
   else
-    'Tie game!'
+    :tie
   end
 end
 
@@ -38,7 +65,7 @@ end
 def solicit_choice
   loop do
     prompt "Choose one: #{VALID_CHOICES.values.join ', '}"
-    choice = Kernel.gets.chomp
+    choice = Kernel.gets.chomp.downcase
     choice = VALID_CHOICES[choice] if VALID_CHOICES.key? choice
     return choice if VALID_CHOICES.values.include? choice
 
@@ -55,14 +82,32 @@ end
 
 #-----------------------------------------------------------------------------
 # Main processing. Keeps repeating until user doesn't want to continue.
+
+prompt MESSAGES[:welcome]
+prompt MESSAGES[:abbreviations]
+scores = Hash.new 0
+
 loop do
   player_choice = solicit_choice
   computer_choice = VALID_CHOICES.values.sample
   prompt "You chose #{player_choice}. Computer chose #{computer_choice}."
-  prompt results(player_choice, computer_choice)
 
-  prompt "Do you want to play again?"
-  answer = gets.chomp
-  break unless answer.start_with? 'y'
+  round_result = results(player_choice, computer_choice)
+  if round_result == :you_won
+    scores[:player] += 1
+  elsif round_result == :computer_won
+    scores[:computer] += 1
+  end
+  prompt MESSAGES[round_result]
+  prompt "Score is:  you #{scores[:player]}    me #{scores[:computer]}"
+
+  break if game_over? scores
 end
+
+if scores[:player] > scores[:computer]
+  prompt "Congratulations. You won!"
+else
+  prompt "Sorry. You lost. Go me!"
+end
+
 prompt "Bye! Thanks for playing."
