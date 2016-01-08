8328f8a0af396e67e5219a2641482404b5697c30
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-07 16:29:34 -0800

    Play repeatedly.

--- L2/rps.rb
+++ L2/rps.rb
@@ -4,25 +4,30 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-choice = ''
 loop do
-  prompt "Choose one: #{VALID_CHOICES.join ', '}"
-  choice = Kernel.gets.chomp
-  break if VALID_CHOICES.include? choice
-  prompt "That is not a valid choice."
-end
+  choice = ''
+  loop do
+    prompt "Choose one: #{VALID_CHOICES.join ', '}"
+    choice = Kernel.gets.chomp
+    break if VALID_CHOICES.include? choice
+    prompt "That is not a valid choice."
+  end
 
-computer_choice = VALID_CHOICES.sample
-if (choice == 'rock' && computer_choice == 'scissors') ||
-   (choice == 'paper' && computer_choice == 'rock') ||
-   (choice == 'scissors' && computer_choice == 'paper')
-  prompt 'You won!'
-elsif (choice == 'rock' && computer_choice == 'paper') ||
-      (choice == 'paper' && computer_choice == 'scissors') ||
-      (choice == 'scissors' && computer_choice == 'rock')
-  prompt 'You lost!'
-else
-  prompt 'Tie game!'
-end
+  computer_choice = VALID_CHOICES.sample
+  if (choice == 'rock' && computer_choice == 'scissors') ||
+     (choice == 'paper' && computer_choice == 'rock') ||
+     (choice == 'scissors' && computer_choice == 'paper')
+    prompt 'You won!'
+  elsif (choice == 'rock' && computer_choice == 'paper') ||
+        (choice == 'paper' && computer_choice == 'scissors') ||
+        (choice == 'scissors' && computer_choice == 'rock')
+    prompt 'Computer won!'
+  else
+    prompt 'Tie game!'
+  end
 
-puts "You chose #{choice}. Computer chose #{computer_choice}."
+  prompt "You chose #{choice}. Computer chose #{computer_choice}."
+  prompt "Do you want to play again?"
+  answer = gets.chomp
+  break unless answer.start_with? 'y'
+end
