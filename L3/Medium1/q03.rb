11b50ccd1d55db72f055c6c43e7a1d88e920eaaf
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-08 17:47:41 -0800

    Medium 1/Question 3

---
+++ L3/Medium1/q03.rb
@@ -0,0 +1,7 @@
+# This is an attempt to add a string and a number; ruby does not permit this.
+
+# Usable method #1
+puts "the value of 40 + 2 is " + (40 + 2).to_s
+
+# Usable method #2
+puts "the value of 40 + 2 is #{40 + 2}"
