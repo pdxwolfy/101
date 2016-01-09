a1e1463b6e62e8e64e35c7130278ff20f8a15dfb
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-09 00:03:56 -0800

    Hard 1/Question 3

---
+++ L3/Hard1/q03b.rb
@@ -0,0 +1,20 @@
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+puts 'Expected output:'
+puts "one is: one"
+puts "two is: two"
+puts "three is: three"
