a6e60fc0c2fb7572a97508688408685b2e694b63
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-09 00:51:10 -0800

    Cleanup

--- L3/Hard1/q05.rb
+++ L3/Hard1/q05.rb
@@ -1,4 +1,4 @@
-def is_a_number?(word)
+def a_number?(word)
   (0..255).include? Integer(word)
 rescue ArgumentError
   false
@@ -7,25 +7,25 @@ end
 def dot_separated_ip_address?(input_string)
   dot_separated_words = input_string.split(".")
   return false unless dot_separated_words.size == 4
-  while dot_separated_words.size > 0 do
+  while dot_separated_words.size > 0
     word = dot_separated_words.pop
-    return false if !is_a_number?(word)
+    return false unless a_number?(word)
   end
-  return true
+  true
 end
 
-def test(value)
-  puts "#{value}: #{dot_separated_ip_address?(value)}"
+def test(value, expect)
+  puts "#{value}: #{dot_separated_ip_address?(value)}   expect #{expect}"
 end
 
-test('1.2.3.4')
-test('255.255.255.255')
-test('255.0.255.255')
-test('255.0.256.255')
-test('192.168.255')
-test('192.168')
-test('192.168.5.10.15')
-test('999.0.0.0')
-test('999.0.0.999')
-test('0.0.0.256')
-test('0.0.0.255')
+test('1.2.3.4', true)
+test('255.255.255.255', true)
+test('255.0.255.255', true)
+test('255.0.256.255', false)
+test('192.168.255', false)
+test('192.168', false)
+test('192.168.5.10.15', false)
+test('999.0.0.0', false)
+test('999.0.0.999', false)
+test('0.0.0.256', false)
+test('0.0.0.255', true)
