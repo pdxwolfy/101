1fc851b4dfac927fc8c162b1203731f71c4b49f6
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-08 18:22:32 -0800

    Medium 1/Question 7

---
+++ L3/Medium1/q07.rb
@@ -0,0 +1,32 @@
+# This will fail because {limit} is a local variable, so is thus not available
+# in the fib() metod.
+#
+# limit = 15
+#
+# def fib(first_num, second_num)
+#   while second_num < limit
+#     sum = first_num + second_num
+#     first_num = second_num
+#     second_num = sum
+#   end
+#   sum
+# end
+#
+# result = fib(0, 1)
+# puts "result is #{result}"
+#
+# To fix it, convert {limit} to a CONSTANT.
+
+LIMIT = 15
+
+def fib(first_num, second_num)
+  while second_num < LIMIT
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1)
+puts "result is #{result}"
