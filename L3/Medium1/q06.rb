2ae27eae5d8391ec4ff0e2e41d1771b55ece8126
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-08 18:10:45 -0800

    Medium 1/Question 6

---
+++ L3/Medium1/q06.rb
@@ -0,0 +1,14 @@
+def rolling_buffer1(buffer, max_buffer_size, new_element)
+  buffer << new_element
+  buffer.shift if buffer.size >= max_buffer_size
+  buffer
+end
+
+def rolling_buffer2(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size >= max_buffer_size
+  buffer
+end
+
+# rolling_buffer1() mutates the contents of Array passed in as {buffer}.
+# rolling_buffer2() does not mutate the original Array.
