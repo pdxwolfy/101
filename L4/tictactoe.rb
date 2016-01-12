1e30d952d2b07a8bbe82431c80f6d3abf726e56d
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-12 15:48:20 -0600

    Variable sized board.

--- L4/tictactoe.rb
+++ L4/tictactoe.rb
@@ -2,83 +2,83 @@
 # Pete Hanson
 
 INITIAL_MARKER = ' '
-PLAYER_MARKER = 'X'
-COMPUTER_MARKER = 'O'
-TIE_GAME = 'T'
-
-BOARD_SIZE = 3
-WINNING_ROWS = %w(123 456 789 147 258 369 159 357)
+PLAYER_MARKER = :X
+COMPUTER_MARKER = :O
+THE_PLAYERS = [PLAYER_MARKER, COMPUTER_MARKER]
+TIE_GAME = :T
 WINNING_SCORE = 5
 
 MESSAGES = {
-  congratulations: 'You won. Congratulations!',
-  continue:        'Press [Return] for next game, or type Q to quit.',
-  i_selected:      'I selected square number %{move}',
-  instructions:    "You are an #{PLAYER_MARKER}. " \
-                   "The computer is an #{COMPUTER_MARKER}\n" \
-                   'You may enter Q at any time to quit.',
-  invalid_square:  'Invalid: that is the not the number of a square.',
-  overall_loss:    'You lost %{#{COMPUTER_MARKER}} games to ' \
-                   '%{#{PLAYER_MARKER}} with %{#{TIE_GAME} tie games. Sorry!',
-  overall_tie:     'We tied at %{#{PLAYER_MARKER}} games to ' \
-                   '%{#{COMPUTER_MARKER}} with %{#{TIE_GAME}} tie games.',
-  overall_win:     'You won %{#{PLAYER_MARKER}} games to ' \
-                   '%{#{COMPUTER_MARKER}} with %{#{TIE_GAME} tie games}. ' \
-                   'Congratulations!',
-  play_prompt:     'Please enter your move: %{available}',
-  numbering:       "Squares are numbered as:\n\n",
-  score:           'Overall scores:   you: %d   me: %d   ties: %d',
-  square_in_use:   'That square is in use.',
-  tie_game:        'Tie game. Sigh.',
-  you_lost:        'Tic! Tac! Toe! You lost. Sorry!'
+  bad_board_magnitude:  'Board size should be between 3 and 9, inclusive.',
+  cell_not_available:   'That square is either not available or invalid.',
+  congratulations:      'You won. Congratulations!',
+  continue:             'Press [Return] for next game, or type Q to quit.',
+  get_board_magnitude:  'What size board do you want (3-9)?',
+  i_selected:           'I selected square number %{move}',
+  instructions:         "You are an #{PLAYER_MARKER}. " \
+                        "The computer is an #{COMPUTER_MARKER}\n" \
+                        'You may enter Q at any time to quit.',
+  overall_loss:         "You lost %{#{COMPUTER_MARKER}} games to " \
+                        "%{#{PLAYER_MARKER}} with %{#{TIE_GAME}} " \
+                        'tie games. Sorry!',
+  overall_tie:          "We tied at %{#{PLAYER_MARKER}} games to " \
+                        "%{#{COMPUTER_MARKER}} with %{#{TIE_GAME}} " \
+                        'tie games.',
+  overall_win:          "You won %{#{PLAYER_MARKER}} games to " \
+                        "%{#{COMPUTER_MARKER}} with %{#{TIE_GAME}} " \
+                        'tie games. Congratulations!',
+  play_prompt:          'Please enter your move: %{available}',
+  numbering:            "Squares are numbered as:\n\n",
+  score:                "Overall scores:   you: %{#{PLAYER_MARKER}}   " \
+                        "me: %{#{COMPUTER_MARKER}}   ties: %{#{TIE_GAME}}",
+  tie_game:             'Tie game. Sigh.',
+  you_lost:             'Tic! Tac! Toe! You lost. Sorry!'
 }
 
 FINAL_GAME_STATUS = {
-  PLAYER_MARKER   => MESSAGES[:congratulations],
-  COMPUTER_MARKER => MESSAGES[:you_lost],
-  TIE_GAME        => MESSAGES[:tie_game]
+  PLAYER_MARKER   => :congratulations,
+  COMPUTER_MARKER => :you_lost,
+  TIE_GAME        => :tie_game
 }
 
 FINAL_MATCH_STATUS = {
-  PLAYER_MARKER   => MESSAGES[:overall_win],
-  COMPUTER_MARKER => MESSAGES[:overall_loss],
-  TIE_GAME        => MESSAGES[:tieoverall_tie]
+  PLAYER_MARKER   => :overall_win,
+  COMPUTER_MARKER => :overall_loss,
+  TIE_GAME        => :overall_tie
 }
 
 #-----------------------------------------------------------------------------
-# Methods to manage board.
+# Methods to manage board and movement key
+
+def board_magnitude(board)
+  (Math.sqrt(board.size) + 0.0001).to_i
+end
 
 def display(board)
   system 'clear'
-  puts MESSAGES[:instructions], ''
-  (1..(BOARD_SIZE * BOARD_SIZE)).each_slice(BOARD_SIZE) do |row|
+  puts message(:instructions), ''
+  ncells = board_magnitude board
+  out = '---+' * (ncells - 1) + '---'
+  (1..board.size).each_slice(ncells) do |row|
+    puts "#{out}      #{out}" unless row[0] == 1
     display_row board, row
   end
 end
 
 def display_row(board, row)
-  draw_line '-----', '+' unless row[0] == 1
-  draw_line '     ', '|'
-  row.each do |square|
-    print '|' unless square % BOARD_SIZE == 1
-    print "  #{board[square.to_s]}  "
-  end
-  puts ''
-  draw_line '     ', '|'
+  print [row.map { |cell| " #{board[cell]} " }].join('|')
+  print '      '
+  puts [row.map { |cell| format(' %-2s', cell) }].join('|')
 end
 
-def draw_line(horizontal, vertical)
-  puts((horizontal + vertical) * (BOARD_SIZE - 1) + horizontal)
-end
-
-def initialize_board
-  (1..9).map { |square| [square.to_s, INITIAL_MARKER] }.to_h
+def initialize_board(magnitude)
+  Hash[(1..(magnitude * magnitude)).map { |n| [n, INITIAL_MARKER] }]
 end
 
 #-----------------------------------------------------------------------------
-# Methods to manage game state
+# Methods to manage/determine game state
 
-def empty_squares(board)
+def empty_cells(board)
   board.select { |_, status| status == INITIAL_MARKER }.keys
 end
 
@@ -86,37 +86,54 @@ def game_over?(board)
   winner(board) || tied_game?(board)
 end
 
-# Returns PLAYER_MARKER, COMPUTER_MARKER, TIE_GAME, or nil
 def game_status(board)
   status = winner board
-  return status if status
-  return TIE_GAME if tied_game? board
-  nil
+  if status
+    status
+  elsif tied_game? board
+    TIE_GAME
+  end
 end
 
 def match_over?(scores)
-  highest_score = scores.values_at(PLAYER_MARKER, COMPUTER_MARKER).max
-  return true if highest_score >= WINNING_SCORE
-  puts MESSAGES[:continue]
+  return true if scores.values_at(*THE_PLAYERS).max >= WINNING_SCORE
+  puts message(:continue)
   print '> '
   gets.chomp.downcase == 'q'
 end
 
 def tied_game?(board)
-  empty_squares(board).empty?
+  empty_cells(board).empty?
 end
 
-# Returns X if X has won the game, O if O has won the game, nil otherwise.
 def winner(board)
-  WINNING_ROWS.each do |row|
-    squares = row.chars.map { |square| board[square] }
-    if squares.count(PLAYER_MARKER) == 3
-      return PLAYER_MARKER
-    elsif squares.count(COMPUTER_MARKER) == 3
-      return COMPUTER_MARKER
-    end
+  magnitude = board_magnitude board
+  winning_lines(board, magnitude).each do |line|
+    cells = line.map { |cell| board[cell] }
+    the_winner = THE_PLAYERS.find { |player| cells.count(player) == magnitude }
+    return the_winner if the_winner
   end
   nil
+end
+
+def winning_columns(board, magnitude)
+  (1..magnitude).map { |n| (n..board.size).step(magnitude).to_a }
+end
+
+def winning_diagonals(board, magnitude)
+  diag1 = (1..board.size).step(magnitude + 1).to_a
+  diag2 = (magnitude..board.size - 1).step(magnitude - 1).to_a
+  [diag1, diag2]
+end
+
+def winning_lines(board, magnitude)
+  winning_rows(board, magnitude) +
+    winning_columns(board, magnitude) +
+    winning_diagonals(board, magnitude)
+end
+
+def winning_rows(board, magnitude)
+  (1..board.size).each_slice(magnitude).to_a
 end
 
 #-----------------------------------------------------------------------------
@@ -138,20 +155,23 @@ def report_final_score(scores)
                else
                  TIE_GAME
                end
-  puts format(FINAL_MATCH_STATUS[the_winner], scores)
+  puts message(FINAL_MATCH_STATUS[the_winner], scores)
 end
 
 def report_score(scores)
-  the_scores = scores.values_at(PLAYER_MARKER, COMPUTER_MARKER, TIE_GAME)
-  puts format(MESSAGES[:score], *the_scores)
+  puts message(:score, scores)
 end
 
 #-----------------------------------------------------------------------------
 # Methods to manage play
 
+def choose_move(board)
+  empty_cells(board).sample
+end
+
 def computer_move!(board)
-  move = empty_squares(board).sample
-  puts format(MESSAGES[:i_selected], move: move)
+  move = choose_move(board)
+  puts message(:i_selected, move: move)
   board[move] = COMPUTER_MARKER
 end
 
@@ -164,14 +184,14 @@ def play_game!(board)
   end
 end
 
-def play_match
+def play_match(board_magnitude)
   scores = initialize_scores
   loop do
-    board = initialize_board
+    board = initialize_board board_magnitude
     display board
     play_game! board
     the_winner = game_status board
-    puts '', FINAL_GAME_STATUS[the_winner]
+    puts '', message(FINAL_GAME_STATUS[the_winner])
     record_score! scores, the_winner
     report_score scores
     break if match_over? scores
@@ -181,33 +201,44 @@ def play_match
 end
 
 def player_move!(board)
-  move = ''
-  available_squares = empty_squares board
-  available_numbers = joinor(available_squares)
+  available_cells = empty_cells board
+  available_numbers = joinor(available_cells)
   loop do
-    puts '', format(MESSAGES[:play_prompt], available: available_numbers)
-    show_numbers
+    puts '', message(:play_prompt, available: available_numbers)
     print "> "
-    move = gets.chomp
-    break if available_squares.include? move
-    puts MESSAGES[move.match(/^\d$/) ? :square_in_use : :invalid_square]
+    move = to_integer(gets.chomp)
+    if available_cells.include?(move)
+      board[move] = PLAYER_MARKER
+      break
+    end
+    puts message(:cell_not_available)
   end
-  board[move] = PLAYER_MARKER
 end
 
 #-----------------------------------------------------------------------------
 # Miscellaneous methods
+
+def message(symbol, args = {})
+  format(MESSAGES[symbol], args)
+end
+
+def to_integer(value)
+  Integer(value)
+rescue ArgumentError
+  nil
+end
 
 def joinor(list, sep = ', ', final = 'or')
   return list.join(sep) if list.size <= 1
   list[0, list.size - 1].join(sep) + "#{sep}#{final} #{list.last}"
 end
 
-def show_numbers
-  format_string = (BOARD_SIZE <= 3) ? '%d' : '%2d'
-  (1..(BOARD_SIZE * BOARD_SIZE)).each_slice(BOARD_SIZE).map do |row|
-    puts "    #{row.map { |n| format(format_string, n) }.join ' '}"
-  end
+board_magnitude = nil
+loop do
+  puts message(:get_board_magnitude)
+  board_magnitude = to_integer(gets.chomp)
+  break if board_magnitude && (3..10).include?(board_magnitude)
+  puts message(:bad_board_magnitude)
 end
 
-play_match
+play_match board_magnitude
