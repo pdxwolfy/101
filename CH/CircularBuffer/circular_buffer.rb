6f12179eb9f3729ae9d1f50f052ece55bcfd07c1
Pete Hanson <wolfy@pdxwolfy.org>
2016-01-22 21:04:09 -0600

    Rework to actually work.

--- CH/CircularBuffer/circular_buffer.rb
+++ CH/CircularBuffer/circular_buffer.rb
@@ -6,32 +6,46 @@ class CircularBuffer
 
   def initialize(number_of_cells)
     @buffer = Array.new(number_of_cells)
-    @read = 0
-    @write = 0
+    @read_index = @write_index = 0
   end
 
   def advance(pointer)
-    @buffer.size ? 0 : (pointer + 1)
+    pointer += 1
+    (pointer == @buffer.size) ? 0 : pointer
   end
 
   def empty?
-    @write == @read
+    @read_index == @write_index && @buffer[@read_index].nil?
   end
 
   def full?
-    advance(@write) == @read
+    @read_index == @write_index && @buffer[@read_index]
+  end
+
+  def clear
+    @buffer.fill(nil)
+    @read_index = @write_index = 0
   end
 
   def read
     fail(BufferEmptyException) if empty?
-    result = @buffer[@read]
-    @read = advance(@read)
+    result = @buffer[@read_index]
+    @buffer[@read_index] = nil
+    @read_index = advance(@read_index)
     result
   end
 
   def write(value)
+    return if value.nil?
     fail(BufferFullException) if full?
-    @buffer[@write] = value
-    @write = advance(@write)
+    @buffer[@write_index] = value
+    @write_index = advance(@write_index)
+  end
+
+  def write!(value)
+    return if value.nil?
+    @read_index = advance(@read_index) if full?
+    @buffer[@write_index] = value
+    @write_index = advance(@write_index)
   end
 end
--- CH/CircularBuffer/test_circular_buffer.rb
+++ CH/CircularBuffer/test_circular_buffer.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require_relative 'circular_buffer'
+require 'pry'
 
 class CircularBufferTest < Minitest::Test
   def test_read_empty_buffer_throws_buffer_empty_exception
@@ -8,7 +9,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_write_and_read_back_one_item
-    skip
     buffer = CircularBuffer.new(1)
     buffer.write '1'
     assert_equal '1', buffer.read
@@ -16,7 +16,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_write_and_read_back_multiple_items
-    skip
     buffer = CircularBuffer.new(2)
     buffer.write '1'
     buffer.write '2'
@@ -26,7 +25,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_clearing_buffer
-    skip
     buffer = CircularBuffer.new(3)
     ('1'..'3').each { |i| buffer.write i }
     buffer.clear
@@ -39,7 +37,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_alternate_write_and_read
-    skip
     buffer = CircularBuffer.new(2)
     buffer.write '1'
     assert_equal '1', buffer.read
@@ -48,7 +45,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_reads_back_oldest_item
-    skip
     buffer = CircularBuffer.new(3)
     buffer.write '1'
     buffer.write '2'
@@ -59,7 +55,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_writes_of_nil_should_not_occupy_buffer
-    skip
     buffer = CircularBuffer.new(5)
     buffer.write nil
     ('1'..'3').each { |i| buffer.write i }
@@ -67,7 +62,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_writing_to_a_full_buffer_throws_an_exception
-    skip
     buffer = CircularBuffer.new(2)
     buffer.write '1'
     buffer.write '2'
@@ -75,7 +69,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_overwriting_oldest_item_in_a_full_buffer
-    skip
     buffer = CircularBuffer.new(2)
     buffer.write '1'
     buffer.write '2'
@@ -86,7 +79,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_forced_writes_of_nil_should_not_occupy_buffer
-    skip
     buffer = CircularBuffer.new(2)
     ('1'..'2').each { |i| buffer.write i }
     buffer.write! nil
@@ -96,7 +88,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_forced_writes_to_non_full_buffer_should_behave_like_writes
-    skip
     buffer = CircularBuffer.new(2)
     buffer.write '1'
     buffer.write! '2'
@@ -107,7 +98,6 @@ class CircularBufferTest < Minitest::Test
 
   # rubocop:disable Metrics/MethodLength
   def test_alternate_read_and_write_into_buffer_overflow
-    skip
     buffer = CircularBuffer.new(5)
     ('1'..'3').each { |i| buffer.write i }
     buffer.read
